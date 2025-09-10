467f0ef57e9759eff5b8780e3607f13376187fde
thuthuhnin-9984 <leeh67933@gmail.com>
2025-09-11 00:20:52 +0800

    Create OutputSpecialCharacters.java

---
+++ OutputSpecialCharacters.java
@@ -0,0 +1,32 @@
+public class OutputSpecialCharacters {
+    public static void main(String[] args) {
+
+        System.out.print("Demonstrating Special Characters in Java\n");
+        System.out.print("---------------------------------------\n");
+
+        // New line
+        System.out.print("New line example:\nHello\nWorld\n");
+
+        // Tab
+        System.out.print("Tab example:\tJava\tProgramming\t\n\n");
+
+        // Backslash
+        System.out.print("Backslash: \\\n");
+
+        // Double quote
+        System.out.print("Double quote: \"Hello\"\n");
+
+        // Single quote
+        System.out.print("Single quote: \'A\'\n");
+
+        // Carriage return
+        System.out.print("Carriage return example:\n");
+        System.out.print("Original Line\rOverwritten line\n");
+
+        // Backspace
+        System.out.print("Backspace example: ABC\bD\n");
+
+        // Form feed (effect depends on system/console)
+        System.out.print("Form feed example: First page\fSecond page\n");
+    }
+}
